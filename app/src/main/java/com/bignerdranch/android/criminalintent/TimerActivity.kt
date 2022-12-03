165637bba45938c8f859cd608e5e22971829e259
BitComing <livov@foxmail.com>
2022-12-03 17:18:42 +0800

    Initial 计时器初始化

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/TimerActivity.kt
@@ -0,0 +1,11 @@
+package com.bignerdranch.android.criminalintent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TimerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_timer)
+    }
+}
