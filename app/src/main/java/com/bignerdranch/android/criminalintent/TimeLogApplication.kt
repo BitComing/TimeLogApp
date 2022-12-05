722567ae30be9e5666e890b746f180d527da0175
BitComing <livov@foxmail.com>
2022-12-05 18:53:34 +0800

    Update file name

--- app/src/main/java/com/bignerdranch/android/criminalintent/TimeLogApplication.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/TimeLogApplication.kt
@@ -2,7 +2,7 @@ package com.bignerdranch.android.criminalintent
 
 import android.app.Application
 
-class CriminalIntentApplication : Application() {
+class TimeLogApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         CrimeRepository.initialize(this)
